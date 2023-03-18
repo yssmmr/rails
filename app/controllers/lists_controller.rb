a5b155ff8883ef2377dca025a4ed9c60830ca5a1
yssmmr <125275634+yssmmr@users.noreply.github.com>
2023-03-18 12:48:29 +0000

    [Add]4-12フラッシュメッセージ追記

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,15 +5,13 @@ class ListsController < ApplicationController
   end
 
   def create
-    #1 .&2.データを受け取り新規登録するためのインスタンス作成
-    @list = List.new(list_params)
-    #3. データをデータベースに保存するためのsaveメソッド実行
-    if @list.save
-      redirect_to list_path(@list.id)
-    #4. 詳細画面へリダイレクト
-    else
-      render :new
-    end
+    #1.データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    #2. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    #3.フラッシュメッセージを定義し、詳細画面へリダイレクト
+    flash[:notice] = "投稿が成功しました"
+    redirect_to list_path(list.id)
   end
 
   def index
